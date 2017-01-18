f1b5c43d96d5d9c90d4a11c76a33afcfe1422199
d0evi1 <3331000@qq.com>
2017-01-18 18:45:02 +0800

    up.

--- exptable.py
+++ exptable.py
@@ -19,13 +19,21 @@ expTable = [0 for i in range(0, EXP_TABLE_SIZE)]
 
 print len(expTable)
 
+# 初始化。
 for i in range(0, EXP_TABLE_SIZE):
-    expTable[i] = math.exp(i * 1.0 / EXP_TABLE_SIZE * 2 - 1) * MAX_EXP
+    expTable[i] = math.exp((i * 1.0 / EXP_TABLE_SIZE * 2 - 1) * MAX_EXP)
     expTable[i] = expTable[i] / (expTable[i] + 1)
 
-
-
 print expTable
+
+## 此处f 范围(-6,6)用f/1000.0来代替. 
+m = [int((f/1000.0 + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2)) for f in range(-6000, 6000)]
+
+print m
+
+
+result = [expTable[x] for x in m]
+#print result
 
 fig = plt.figure()
 
@@ -35,15 +43,15 @@ ax = plt.subplot(1,1,1)
 #plt.ylim(ymax=1.2)
 #plt.ylim(ymin=0.5)
 
-ax.plot(expTable)
+ax.plot(result)
+
+#ax.plot(expTable)
 #ax.plot(expTable, label="exp")
 
 handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles[::-1], labels[::-1])
 
-f = [((i + MAX_EXP) * (EXP_TABLE_SIZE / MAX_EXP / 2)) for i in range(0,100)]
-print f
-
+# python自动取整.
 
 plt.show()
 
